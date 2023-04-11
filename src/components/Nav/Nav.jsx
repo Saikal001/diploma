2ccef3e2c30b38515ff3229d6b725b9360857384
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-11 13:48:24 +0600

    entered some information

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,6 +9,8 @@ export default function Nav() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contacts">Contacts</NavLink>
+        <NavLink to="/delivery">Delivery</NavLink>
+        
       </nav>
       </div>
       );
