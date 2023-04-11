0efc396ab6ede370bfa504246525f69ad80c3c98
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-11 14:44:32 +0600

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDY9Wa0vEjIn27-rc-ePf0rxr5zgk7FL0s",
+  authDomain: "diploma-b7671.firebaseapp.com",
+  projectId: "diploma-b7671",
+  storageBucket: "diploma-b7671.appspot.com",
+  messagingSenderId: "662195838615",
+  appId: "1:662195838615:web:3a37ab99611d6d927293d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
