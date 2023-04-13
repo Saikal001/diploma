a0587e507a02c24b8ed2af1e56d4d5cbff6260ad
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-13 14:45:22 +0600

    Fixed bugs

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -7,7 +7,7 @@ export default function Logo() {
     <div className="Logo">
     
       <NavLink to="/">
-      <img src={logo} alt ="website logo" />
+      <img src="https://i.pinimg.com/564x/23/f7/b1/23f7b19c633b5aa44183beaec39abf37.jpg" alt ="website logo" />
       </NavLink>
       </div>
       );
