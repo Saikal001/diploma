5845daf8c094dabe73d03a0c6b7bee11bfb39508
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-13 12:46:03 +0600

    Imported firebase and got a list of categories

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//получить список категорий (коллекция документов).
+export const categoryCollection = collection(db, 'categories');
