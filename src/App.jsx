c1120ffb037bf49b3e426a173b44bf7214de9a8d
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-13 14:44:35 +0600

    Removed unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import { getDocs } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import { categoryCollection } from "./firebase";
 import About from "./pages/About";
 import Category from "./pages/Category";
 import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
 import Home from "./pages/Home";
 
+export const AppContext = createContext({
+  categories: []
+});
+
 export default function App() {
+const [categories, setCategories] = useState([]);
+//
+useEffect(() => {
+  getDocs(categoryCollection)
+    .then(snapshot => {
+    const newCategories = [];
+
+    snapshot.docs.forEach(doc => {
+      const category = doc.data();
+      category.id = doc.id;
+
+      newCategories.push(category);
+      });
+
+      setCategories(newCategories);
+
+    })
+}, []);
+//
   return (
     <div className="App">
-
+<AppContext.Provider value={{ categories }}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,7 +46,7 @@ export default function App() {
         </Routes>
       </Layout>
 
-
+      </AppContext.Provider>
 
     </div>
   );
