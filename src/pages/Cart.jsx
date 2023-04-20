8ccf09536f8ff5dd80a3b6f8c3025c3b1507898b
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-20 14:03:27 +0600

    Copied Cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import CartList from "../components/CartList/CartList";
+
+export default function CartLink() {
+  return(
+    <div className="Cart">
+      <h1>Cart</h1>
+      <CartList/>
+    </div>
+  );
+  
+}
