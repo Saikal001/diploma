869e10e5f0efaf843b3f19a5c9bf78c2d48a011a
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-20 15:14:48 +0600

    Added getAuth and imported

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
-
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDY9Wa0vEjIn27-rc-ePf0rxr5zgk7FL0s",
@@ -15,7 +16,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app)
 
 //получить список категорий (коллекция документов).
 export const categoryCollection = collection(db, 'categories');
 export const productCollection = collection(db, 'products');
+
+
+
+const provider = new GoogleAuthProvider();
+export const logIn = () => signInWithPopup(auth, provider);
+export const logOut = () => signOut(auth);
+export const onAuthChange = (callback) = onAuthStateChanged(auth, callback);
