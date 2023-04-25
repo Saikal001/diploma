e1753e1a88a01c344f0f651a97d60d46dbbb6bcb
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-04-25 15:34:48 +0600

    Created files and added some changes

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,11 @@
+import OrderList from "../components/OrderList/OrderLiist";
+
+
+export default function Orders() {
+  return (
+    <div className="Orders">
+      <h1>Orders</h1>
+      < OrderList />
+    </div>
+  )
+}
