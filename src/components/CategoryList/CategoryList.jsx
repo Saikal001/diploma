f17e10050c5ddb9bc8b8f3c937cccc3d06d8a10e
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-12 14:14:51 +0600

    Added and corrected errors

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -38,10 +38,36 @@ export default function CategoryList() {
   ))
   return (
     <div className="CategoryList">
-      <ul>
+      
+      <button className="dropdownBtn"> Categories <i className="fa-solid fa-chevron-down fa-xs"></i></button>     <ul className="dropdownContent">
         {output}
       </ul>
       <AddCategory/>
     </div>
   );
+
+
 }
+
+// export default function CategoryList() {//
+//   const { categories } = useContext(AppContext);//
+
+//   const output = categories.map((category) => (
+//     <li key={category.id}>//
+//       <NavLink to={"/categories/" + category.slug}>
+//         {category.name}
+//       </NavLink>//
+
+//       <DeleteCategory category={category} />
+//     </li>
+//   ))
+
+//   return (
+//     <div className="CategoryList">
+//<button className="dropdownBtn"> Categories <i className="fa-solid fa-chevron-down fa-xs"></i></button>
+//       <ul className="dropdownContent">{       output}
+//       <AddCategory />
+//       </ul>
+//     </div>
+//   )
+// }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Welcome to my Sweets shop</h1>
-      <p> This is my awesome Sweets shop.</p>
-      <p>There are different types of sweets</p>
+      <h1><b>Welcome to my Sweets shop</b></h1>
+      <p><strong> This is my awesome Sweets shop</strong></p>
+      <p><strong>There are different types of sweets</strong></p>
       
       </div>
       );
