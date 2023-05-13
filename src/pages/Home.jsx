34202db7ceebee42c94a28e84b9f181ee83602a4
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-13 17:52:54 +0600

    Delete padding in ProductList.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,11 @@ export default function Home() {
       <h1><b>Welcome to my Sweets shop</b></h1>
       <p><strong> This is my awesome Sweets shop</strong></p>
       <p><strong>There are different types of sweets</strong></p>
+    
+  
+</div>
       
-      </div>
+      
+      
       );
       }
