7fdfa190ec8bffbd245a2bb521d5194337d6c3ac
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-16 14:52:02 +0600

    Created new pages

---
+++ src/pages/Holiday.jsx
@@ -0,0 +1,44 @@
+import CategoryList from "../components/CategoryList/CategoryList";
+import "./Holiday.css";
+
+
+export default function Home() {
+  return (
+    <div className="Holiday">
+<div class="">
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <h1><b>Holiday sweet eating competitions and distribution of sweets to children</b></h1>
+      {/* <aside>
+        <CategoryList />
+      </aside> */}
+      <h2>On World Chocolate Day, which was celebrated this weekend, a speed chocolate eating contest was help for children.</h2>
+      <p>Seveeral venues were organized in the city various evebts were held.
+        For the near with Sweet Shop , and the Chocolate Show holiday-game took place on the main site - in the children's library. In addition to an interesting and exstraordinarily tasty completition for speed eating chocolate, as well as the "Dress Up" contest, where children dressed each other with candies using clothespins, theorganizers arranged an exhibiton of sweets ,watching a video about choocolate production.
+        Also , the sweet tooth was toldabout the history.
+
+      </p>
+    </div>
+
+
+
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,31 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h1><b>Welcome to my Sweets shop</b></h1>
+<video>
+  <source src="https://pin.it/4NNQV0E" />
+</video>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <form>
+      <p><input type="button" value="Start for free" /></p>
+      </form>
+
+
+      {/* <h1><b>Welcome to my Sweets shop</b></h1>
       <p><strong> This is my awesome Sweets shop</strong></p>
-      <p><strong>There are different types of sweets</strong></p>
+      <p><strong>There are different types of sweets</strong></p> */}
     
   
 </div>
