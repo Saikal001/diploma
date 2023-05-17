2543aa967f96aa933f9d18aca0f315d0215a770a
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-17 14:30:05 +0600

    Added content in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import "./Home.css";
+import videoo from "../pages/videoplayback.mp4";
 
 
 export default function Home() {
   return (
     <div className="Home">
-<video>
-  <source src="./" />
-</video>
+
+<div className="video-bg">
+  <video src="video-bg.mp4" type="video/mp4" autoplay muted loop>
+    <div className="effects">
+
+    </div>
+  <source src={videoo} />
+  </video>
+  <div className="video-bg_content">
+    <h1>Sweet</h1>
+
+  </div>
+</div>
+
+
+<div className="main">
+  <h2>Welcome</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus officia pariatur iusto dolorum iste, quam ut accusamus. Soluta eaque officiis possimus dolores veritatis, consequatur aspernatur! Eaque non iure ex maxime.</p>
+</div>
 
 
 
@@ -21,9 +38,32 @@ export default function Home() {
 
 
 
-      <form>
+
+
+
+
+
+
+{/* <video>
+  <source src={videoo} />
+</video> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <form>
       <p><input type="button" value="Start for free" /></p>
-      </form>
+      </form> */}
 
 
       {/* <h1><b>Welcome to my Sweets shop</b></h1>
