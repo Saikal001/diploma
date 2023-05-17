b9342f6b05b6c4a50e2c9d20a7e83324e0101b75
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-17 15:56:32 +0600

    Added information for Blog pages

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,40 +5,44 @@ import "./Blog.css";
 export default function Blog() {
   return (
     <div className="Blog">
-                   <main>
-                    <div className="blog-container">
-                    <div className="blog-header">
-                    <div className="blog-cover">
-13.05.2023
+      <main>
+        <div className="blog-container">
+          <div className="blog-header">
+            <div className="blog-cover">
+              13.05.2023
 
-                      </div>
-                      </div>
-                      <div className="blog-body">
-                        <div className="blog-title">
-                          <h1><a href="#">Lorem ipsum dolor sit amet.</a></h1>
-                        </div>
-<div className="blog-text">
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores ex deserunt provident! Eum, dolores dolorem nemo eligendi delectus laborum at consequatur incidunt distinctio facilis fugiat nulla, porro tenetur non necessitatibus.</p>
-</div>
-<div className="blog-tags">
-  <ul>
-    <li><a href="#">lorem1</a></li>
-    <li><a href="#">lorem2</a></li>
-    <li><a href="#">lorem3</a></li>
-    <li><a href="#">lorem4</a></li>
-    <li><a href="#">lorem5</a></li>
-    </ul>
-</div>
-                      </div>
-                      <div className="blog-footer">
-                        <ul>
-                          <li className="published-date">4 days ago</li>
-                        </ul>
-                      </div>
-                    </div>
-                    </main>   
+            </div>
+          </div>
+          <div className="blog-body">
+            <div className="blog-title">
+              <h1><a href="#">New items and menu updates</a></h1>
+            </div>
+            <div className="blog-text">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores ex deserunt provident! Eum, dolores dolorem nemo eligendi delectus laborum at consequatur incidunt distinctio facilis fugiat nulla, porro tenetur non necessitatibus.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis laudantium dolore inventore explicabo soluta accusamus quo nam officiis? Earum corporis deserunt dignissimos distinctio ex! Ex doloremque explicabo laudantium nobis voluptate.
+                Veritatis quidem cupiditate aliquid impedit magni natus, error possimus quasi itaque tenetur. Mollitia exercitationem beatae fugit necessitatibus soluta amet repellendus quo maiores vel laborum, nostrum deserunt porro explicabo rem commodi!
+                Quod, corporis quos in provident quasi aperiam officia iste esse ad ratione animi laborum dicta quae temporibus! Labore vel earum repellendus, aut, natus fuga maiores alias dolore enim iste voluptate.
+              </p>
+            </div>
+            <div className="blog-tags">
+              <ul>
+                <li><a href="#">news</a></li>
+                <li><a href="#">history</a></li>
+                <li><a href="#">sensation</a></li>
+                <li><a href="#">blog</a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="blog-footer">
+            <ul>
+              <li className="published-date">4 days ago</li>
+            </ul>
+          </div>
+        </div>
+      </main>
 
-      
+
     </div>
 
 
