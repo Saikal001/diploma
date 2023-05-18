84d4d9244eeafd33d3750a71d1f794de7a20589b
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-18 12:24:42 +0600

    Removed Delivery import from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Holiday from "./pages/Holiday";
-import Delivery from "./pages/Delivery";
 import About from "./components/About/About";
 import Category from "./pages/Category";
 import { createContext, useEffect, useState } from "react";
@@ -15,6 +14,9 @@ import ThankYou from "./pages/ThankYou";
 import Orders from "./pages/Orders";
 import Privacy from "./pages/Privacy";
 import Blog from "./pages/Blog";
+
+
+
 
 export const AppContext = createContext({
   categories: [],
@@ -65,7 +67,7 @@ export default function App() {
             <Route path="/about" element={<About/>} />
             <Route path="/holiday" element={<Holiday/>} />
             <Route path="/privacy" element={<Privacy />} />
-            <Route path="/delivery" element={<Delivery />} />
+            
             <Route path="/category/:path" element={<Category />} />
             <Route path="/product/:path" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
