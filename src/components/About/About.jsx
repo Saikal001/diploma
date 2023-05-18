7a248e9180af5203b9205e6dda92648fc45cd3c9
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-18 12:27:52 +0600

    Deleted the offer in About.js

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="About">
       <h1>Welcome to my Sweets shop</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      
       
 
       <div className="simple-spinner"></div>
