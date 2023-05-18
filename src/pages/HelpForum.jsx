7815c58cda8baf16d2176cc442f557d5ba625899
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-18 21:23:16 +0600

    Started adding content for HelpFirum.jsx

---
+++ src/pages/HelpForum.jsx
@@ -0,0 +1,25 @@
+import "./HelpForum.css";
+
+
+export default function HelpForum() {
+  return (
+    <div className="HelpForum">
+
+<div className="intro">
+<div className="cantainer">
+<h1>Help forum</h1>
+</div>
+</div>
+
+
+
+
+
+
+
+    </div>
+
+
+
+  );
+}
