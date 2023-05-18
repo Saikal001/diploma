81c1bbd4ac0b1b0b524824e8045b8528502236a5
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-18 18:51:29 +0600

    Created a new component where new products will be displayed

---
+++ src/components/Popular/Popular.jsx
@@ -0,0 +1,15 @@
+import  "./Popular.css";
+
+
+export default function Popular() {
+  return (
+    <div className="Popular">
+      
+
+      <div className="main-text">Who loves to read?</div>
+
+      <img src={popular} alt="popular"/>
+
+      </div>
+      );
+      }
