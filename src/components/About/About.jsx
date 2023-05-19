a1e2010159acb862183e880ca3faf052f2abc588
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-19 14:31:30 +0600

    Changed some parts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import "./About.css";
+import img from "../../assets/liza.jpg";
 
 export default function About() {
   return (
@@ -6,36 +7,36 @@ export default function About() {
       {/* <h1>About</h1> */}
       <div className="section">
         <div className="container">
-          <div className="content-section"></div>
-<div className="title">
-  <h1>About Us</h1>
+          <div className="content-section">
+          <div className="title">
+            <h1>About</h1>
+          </div>
+          <div className="content">
+            <h3>Lorem ipsum dolor sit.</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit illum animi asperiores id. Accusantium cupiditate adipisci pariatur est dignissimos, quibusdam aliquam? Consequatur quod ipsam soluta maxime nemo. Quidem, quibusdam doloribus.
+              Doloribus delectus distinctio magnam debitis adipisci eveniet tempore, quasi, vitae animi quaerat nostrum consectetur aspernatur laudantium dolores aliquam incidunt unde non at consequuntur. Quidem quibusdam consequatur voluptatem. Quasi, mollitia reprehenderit?
+              Fuga, voluptates itaque. Laborum neque, nobis officiis distinctio eum quod fugit eius! Dolorem optio, eveniet culpa corporis tenetur minima amet provident! Velit temporibus ducimus provident quaerat error, asperiores corporis eius.
+              Qui ipsa molestias nam in ea provident facilis voluptates quae repellendus sit sapiente cupiditate debitis nisi, enim amet ut officia vero inventore odio alias optio quia. Quod nostrum doloremque ducimus.
+              Rem neque veritatis facere? Asperiores, magni. Dolore distinctio a autem cupiditate corporis amet, praesentium voluptates? Ullam quam, consectetur sapiente dolores nobis magni earum iste repellendus tenetur in totam explicabo minus.</p>
+
+            <div className="button">
+              <a href="">Read more</a>
+            </div>
+          </div>
+          <div className="social">
+            <a href=""><i className="fab fa-facebook-f"></i></a>
+            <a href=""><i className="fab fa-twitter-f"></i></a>
+            <a href=""><i className="fab fa-instagram-f"></i></a>
+          </div>
+        </div>
+        
+        <div className="image-section">
+
+          <img src={img} alt="img" />
+        </div>
+
+      </div>
 </div>
-<div className="content">
-        <h3>Lorem ipsum dolor sit.</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit illum animi asperiores id. Accusantium cupiditate adipisci pariatur est dignissimos, quibusdam aliquam? Consequatur quod ipsam soluta maxime nemo. Quidem, quibusdam doloribus.
-        Doloribus delectus distinctio magnam debitis adipisci eveniet tempore, quasi, vitae animi quaerat nostrum consectetur aspernatur laudantium dolores aliquam incidunt unde non at consequuntur. Quidem quibusdam consequatur voluptatem. Quasi, mollitia reprehenderit?
-        Fuga, voluptates itaque. Laborum neque, nobis officiis distinctio eum quod fugit eius! Dolorem optio, eveniet culpa corporis tenetur minima amet provident! Velit temporibus ducimus provident quaerat error, asperiores corporis eius.
-        Qui ipsa molestias nam in ea provident facilis voluptates quae repellendus sit sapiente cupiditate debitis nisi, enim amet ut officia vero inventore odio alias optio quia. Quod nostrum doloremque ducimus.
-        Rem neque veritatis facere? Asperiores, magni. Dolore distinctio a autem cupiditate corporis amet, praesentium voluptates? Ullam quam, consectetur sapiente dolores nobis magni earum iste repellendus tenetur in totam explicabo minus.</p>
-
-        <div className="button">
-          <a href="">Read more</a>
-        </div>
-        </div>
-        <div className="social">
-          <a href=""><i className="fab fa-facebook-f"></i></a>
-          <a href=""><i className="fab fa-twitter-f"></i></a>
-          <a href=""><i className="fab fa-instagram-f"></i></a>
-        </div>
-      </div>
-<div className="image-section">
-  <img src="./rwn72tl74r6erjav9fxtg5myp424t798.jpg" alt="" />
-</div>
-      
-      </div>
-      
-
-      
 
 
 
@@ -50,6 +51,8 @@ export default function About() {
 
 
 
-      </div>
-      );
-      }
+
+
+    </div>
+  );
+}
