88eb285d54cfab8c013525b1a55432e8bddf31c8
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-22 14:07:40 +0600

    Reverted back to previous state due to a bug

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection } from "../../firebase";
-
+import { useContext } from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
 import DeleteCategory from "../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
-      
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    getDocs(categoryCollection)
-      .then(snapshot => {
-      const newCategories = [];
-
-      snapshot.docs.forEach(doc => {
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-        });
-
-        setCategories(newCategories);
-
-      })
-  }, []);
+  const { categories } = useContext(AppContext);
 
   const output = categories.map(category => (
     <li key={category.id}>
@@ -35,39 +15,73 @@ export default function CategoryList() {
       </NavLink>
       <DeleteCategory category={category} />
     </li>
-  ))
+  ));
+
   return (
     <div className="CategoryList">
-      
-      <button className="dropdownBtn"> Categories <i className="fa-solid fa-chevron-down fa-xs"></i></button>     <ul className="dropdownContent">
+      <ul>
         {output}
       </ul>
-      <AddCategory/>
+      <AddCategory />
     </div>
   );
-
-
 }
 
-// export default function CategoryList() {//
-//   const { categories } = useContext(AppContext);//
 
-//   const output = categories.map((category) => (
-//     <li key={category.id}>//
-//       <NavLink to={"/categories/" + category.slug}>
+
+
+
+
+
+
+
+// import { useContext, useEffect, useState } from "react";
+// import { getDocs } from "firebase/firestore";
+// import { categoryCollection } from "../../firebase";
+
+// import "./CategoryList.css";
+// import { NavLink } from "react-router-dom";
+// import AddCategory from "../AddCategory/AddCategory";
+// import DeleteCategory from "../DeleteCategory/DeleteCategory";
+
+// export default function CategoryList() {
+
+//   const [categories, setCategories] = useState([]);
+
+//   useEffect(() => {
+//     getDocs(categoryCollection)
+//       .then(snapshot => {
+//         const newCategories = [];
+
+//         snapshot.docs.forEach(doc => {
+//           const category = doc.data();
+//           category.id = doc.id;
+
+//           newCategories.push(category);
+//         });
+
+//         setCategories(newCategories);
+
+//       })
+//   }, []);
+
+//   const output = categories.map(category => (
+//     <li key={category.id}>
+//       <NavLink to={'/category/' + category.path}>
 //         {category.name}
-//       </NavLink>//
-
+//       </NavLink>
 //       <DeleteCategory category={category} />
 //     </li>
 //   ))
-
 //   return (
 //     <div className="CategoryList">
-//<button className="dropdownBtn"> Categories <i className="fa-solid fa-chevron-down fa-xs"></i></button>
-//       <ul className="dropdownContent">{       output}
-//       <AddCategory />
+
+//       <button className="dropdownBtn"> Categories <i className="fa-solid fa-chevron-down fa-xs"></i></button>     <ul className="dropdownContent">
+//         {output}
 //       </ul>
+//       <AddCategory />
 //     </div>
-//   )
+//   );
+
+
 // }
