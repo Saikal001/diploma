eb10779fd16ad7f3d0c2f4feeff20c9ed43985d3
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-23 15:21:58 +0600

    Left the page blank

--- src/pages/HelpForum.jsx
+++ src/pages/HelpForum.jsx
@@ -3,30 +3,8 @@ import "./HelpForum.css";
 export default function HelpForum() {
   return (
     <div className="HelpForum">
-      {/* <header class="masthead">
-        <div class="conteiner45100">
-
-        </div>
-      </header>
-         */}
-
-      
-
-      
-
-
-
-
-
-
-
-
-
-
-
 
 
 </div>
-      
-      );
-      }
+);
+}
