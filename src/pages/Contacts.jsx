951e84e7076cd461dbc622b0c5ed327096918834
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-23 13:01:06 +0600

    Deleted contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,7 @@
 export default function Contacts() {
   return (
     <div className="Contacts">
-      <h1>Our contacts</h1>
-      <p> +(996) 703 28 30 24 </p>
-      <p> +(996) 703 28 30 24 </p>
-      <p> +(996) 703 28 30 24 </p>
+     
       </div>
       );
       }
