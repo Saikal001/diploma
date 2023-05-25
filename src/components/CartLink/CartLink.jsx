ba7b6530400ed7c47d4d59a99913222e144286ef
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-05-25 16:34:03 +0600

    Deleted picrure

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,4 +1,4 @@
-// import { useContext } from "react";
+
 // import { AppContext } from "../../App";
 // import { Link } from "react-router-dom";
 // import kor from "../assets/kor.jpg";
@@ -25,15 +25,16 @@ import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 
 export default function CartLink() {
-    const { cart } = useContext(AppContext);
-    const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
-    return (
-        <div className="CartLink">
-            <Link to="/cart">
-                <div className="Together">
-                    <img src="https://i.pinimg.com/564x/bd/14/9c/bd149c5c05185de274f040d809f93354.jpg" alt="icon" className="cartIcon" /> <span>({total})</span>
-                </div>
-            </Link>
-        </div>
-    );
+  const { cart } = useContext(AppContext);
+
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
+
+  return (
+    <div className="CartLink">
+      <Link to="/cart">
+        Cart ({total})
+      </Link>
+    </div>
+  );
+    
 }
